e4aa79aaf09cfa33c7069a648f04766b37241442
Chris Hettrick <git@structfoo.com>
2019-05-04 15:50:22 -0700

    Strict command line argument parsing. Add a usage().
    Interactive mode with: $ program_name
    Read from file and print tables: $ program_name file.txt
    Read from file and print tables: $ program_name file.txt nottheword_test
    Read from file and store tables: $ program_name file.txt test
    
    Anything else will print out "usage: program_name [infile [test]]"
    
    Some whitespace fixes.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-/* main.cpp 
+/* main.cpp
    B. Bird - 10/24/2018
 */
 
@@ -14,15 +14,25 @@
 
 using std::string;
 
+static void usage(void);
 
 int main(int argc, char** argv) {
 	string input_str;
-	
-	if(argv[1]){
+	bool test_to_file = false; // Print output to screen by default.
+
+	if (argc == 1) { // Interactive mode.
+		std::cout << ">> ";
+		std::getline(std::cin, input_str);
+	} else if (argc > 1 && argc < 4) { // Read from file mode.
+		// If 2nd arg is "test", then store output to file.
+		if (argc == 3 && strcmp(argv[2], "test") == 0) {
+			test_to_file = true;
+		}
+
 		std::ifstream stdin;
 		stdin.open(argv[1]);
 		if(!stdin){
-			std::cout << "Couldn't open input file" << std::endl;
+			std::cerr << "Couldn't open input file" << std::endl;
 			exit(1);
 		}else{
 			char c;
@@ -32,9 +42,8 @@ int main(int argc, char** argv) {
 				}
 			}
 		}
-	}else{
-		std::cout << ">> ";
-		std::getline(std::cin, input_str);
+	} else { // Not a valid mode.
+		usage();
 	}
 
 	//The below was based on the ANTLR cpp official demo code
@@ -47,13 +56,13 @@ int main(int argc, char** argv) {
 
 	// The next line can be uncommented to see how the parser parses the input statement.
 	// std::cout << tree->toStringTree(&parser) << std::endl << std::endl;
-	
+
 	// Traversing tree with ComputeVisitor
 	SQLComputeVisitor test;
 	std::vector<BaseTblPtr> result = tree->accept(&test);
-	
-	if(strcmp(argv[2], "test") == 0) { // if 2nd arg is "test", then store output to file
-		std::ofstream outfile("./lib/testing/programoutput.csv", 
+
+	if (test_to_file == true) { // Store output to file.
+		std::ofstream outfile("./lib/testing/programoutput.csv",
 			std::ios::out | std::ios::trunc);
 		for (auto cur_tbl: result) {
 			std::vector<std::string> col_names = cur_tbl->get_col_names();
@@ -61,11 +70,19 @@ int main(int argc, char** argv) {
 			store_tbl_contents(cur_tbl, outfile);
 		}
 		outfile.close();
-	} else { // if 2nd arg is not "test", then display output to screen
+	} else { // Print output to screen.
 		for (auto cur_tbl: result) {
 			std::vector<std::string> col_names = cur_tbl->get_col_names();
 			print_col_names(col_names);
 			print_tbl_contents(cur_tbl);
 		}
 	}
+}
+
+static void usage(void)
+{
+	extern char *__progname;
+
+	std::cerr << "usage: " << __progname << " [infile [test]]" << std::endl;;
+	exit(1);
 }
